1bc2e1fd347a5d3e532dc017700e2e061fb9228b
Suriya-vr-46 <suriyaselvam460@gmail.com>
2025-05-10 21:01:25 +0530

    Delete src directory

--- src/database/Database.java
+++
@@ -1,7 +0,0 @@
-package database;
-import java.util.*;
-import model.Bank;
-
-public interface Database {
-	ArrayList<Bank> banks = new ArrayList<>();
-}
--- src/database/ViewDatabase.java
+++
@@ -1,6 +0,0 @@
-package database;
-
-public interface ViewDatabase extends Database{
-	public int showMenu();
-	public void newBank();
-}
